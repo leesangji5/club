f58a3cf89e4bb2d9746109a434b37d9b04d87bcb
sangji <83937801+leesangji5@users.noreply.github.com>
2023-05-15 20:40:15 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+lowerStr = "abcdefghijklmnopqrstuvwxyz"
+upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numStr = "0123456789"
+otherStr = "!@#$%^&*()_+=-`~[]|:;\"\'<>,.?/"
+
+pw = input("Please input your password: ")
+point = 0
+
+point += len(pw) * 0.5
+
+for i in range(len(pw)):
+    if pw[i] in lowerStr:
+        point += 1
+    elif pw[i] in upperStr:
+        point += 1.2
+    elif pw[i] in numStr:
+        point += 1.3
+    elif pw[i] in otherStr:
+        point += 1.5
+    else:
+        point += 2
+
+print(point)
