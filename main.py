045942cee66dd0c8de8a27d9c84b907a0807fb77
sangji <lsgsh211@gmail.com>
2023-06-30 16:20:23 +0900

    add .

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ upperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numStr = "0123456789"
 otherStr = "!@#$%^&*()_+=-`~[]|:;\"\'<>,.?/"
 
-#20p vw
-#4op w
-#60p s
-#80p vs
-
-
 pw = input("Please input your password: ")
 point = 0
 
@@ -29,13 +23,30 @@ for i in range(len(pw)):
     if pw[i] in numStr:
         num += 1
 
+# https://www.uic.edu/apps/strong-password/
+
+# 문자 수 Number of characters
 point += len(pw) * 4
+# 전체 - 대문자 수 Uppercase letters
 point += (len(pw) - upper) * 2
+# 전체 - 소문자 수 Lowercase Letters
 point += (len(pw) - lower) * 2
-if (upper + lower + other) != 0:
-    point += num * 4
+# 숫자 Numbers
+point += num * 4
+# 특수 문자 Symbols
 point += other * 6
 
+# 처음 끝을 제외한 곳의 특수 문자 Middle numbers or symbols
+for j in range(len(pw) - 2):
+    for i in range(len(numStr)):
+        if (pw[j+1] == numStr[i]):
+            point += 2
+    for i in range(len(otherStr)):
+        if (pw[j+1] == otherStr[i]):
+            point += 2
+
+# 조건 충족 Requirements
+# 8자 이상, 대문자, 소문자, 숫자, 특수문자 포함
 if len(pw) >= 8:
     point += 2
 if upper != 0:
@@ -47,17 +58,13 @@ if num != 0:
 if other != 0:
     point += 2
 
-print(pw[2])
-print(numStr[2])
-print(otherStr[2])
-
-for j in range(len(pw) - 2):
-    for i in range(len(numStr)):
-        if (pw[j+1] == numStr[i]):
-            point += 2
-    for i in range(len(otherStr)):
-        if (pw[j+1] == otherStr[i]):
-            point += 2
-
-
 print(point)
+
+if point >= 80:
+    print("Very Strong")
+elif point >= 60:
+    print("Strong")
+elif point >= 40:
+    print("Weak")
+else:
+    print("Very Weak")
